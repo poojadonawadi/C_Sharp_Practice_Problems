d1062b9c7c39ee26e3194403180ac3db7f773f50
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-13 17:35:27 +0530

    [Pooja] added: Number as String

---
+++ NumberAsString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicProblems
+{
+    internal class NumberAsString
+    {
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            string num = Console.ReadLine();
+            int number = int.Parse(num);
+            int result = number * 10;
+            string resultString = Convert.ToString(result);
+            Console.WriteLine("The 10 times of the enter String is: "+resultString);
+        }*/
+    }
+}
