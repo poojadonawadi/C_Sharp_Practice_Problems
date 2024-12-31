c0fbc380b435697a1bd4ad7fa6bdd03f04db2022
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-31 15:34:18 +0530

    [Pooja] added: Average of array elements

---
+++ ArrayAverage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicProblems
+{
+    internal class ArrayAverage
+    {
+        public static double FindAverage(double[] array)
+        {
+            double sum = 0;
+            double avg = 0;
+            for(int i=0;i<array.Length; i++)
+            {
+                sum = sum + array[i];
+                avg = sum / array.Length;
+            }
+            return avg;
+        }
+    }
+}
