b051440df659dd5aaedd967920ec8e0a52902bd8
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-31 15:49:24 +0530

    [Pooja] added: Print n numbers

---
+++ PrintNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicProblems
+{
+    internal class PrintNumbers
+    {
+        private int n { get; }
+        public static void Print1to100(int n)
+        {
+
+            if (n > 100)
+            {
+                Console.WriteLine(n);
+                PrintNumbers.Print1to100(n + 1);
+                return;
+            }
+        }
+    }
+}
